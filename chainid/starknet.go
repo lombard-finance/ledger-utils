ea5258de89fa5519c19c2d20860e952c0aa84e35
hashxtree <145032760+hashxtree@users.noreply.github.com>
2025-12-09 13:39:33 +0300

    avoid pointers

--- chainid/starknet.go
+++ chainid/starknet.go
@@ -8,13 +8,13 @@ import (
 )
 
 type StarknetLChainId struct {
-    lChainId
+	lChainId
 }
 
-func NewStarknetLChainId(id string) (*StarknetLChainId, error) {
+func NewStarknetLChainId(id string) (StarknetLChainId, error) {
 	trimmed := strings.TrimPrefix(id, "0x")
 	if len(trimmed) > ChainIdAvailableLength*2 {
-		return nil, NewMaxErrLength(ChainIdAvailableLength*2, len(id))
+		return StarknetLChainId{}, NewMaxErrLength(ChainIdAvailableLength*2, len(id))
 	}
 	innerChainId, err := newLChainIdFromHex(
 		EcosystemStarknet.ToEcosystemHexByte() +
@@ -22,48 +22,48 @@ func NewStarknetLChainId(id string) (*StarknetLChainId, error) {
 			trimmed,
 	)
 	if err != nil {
-		return nil, err
+		return StarknetLChainId{}, err
 	}
-	return &StarknetLChainId{
+	return StarknetLChainId{
 		lChainId: *innerChainId,
 	}, nil
 }
 
-func NewStarknetLChainIdFromName(name string) (*StarknetLChainId, error) {
+func NewStarknetLChainIdFromName(name string) (StarknetLChainId, error) {
 	trimmed := []byte(strings.TrimSpace(name))
 	if len(trimmed) > ChainIdAvailableLength*2 {
-		return nil, NewMaxErrLength(ChainIdAvailableLength*2, len(trimmed))
+		return StarknetLChainId{}, NewMaxErrLength(ChainIdAvailableLength*2, len(trimmed))
 	}
 	byteChainId := make([]byte, 32)
 	byteChainId[0] = byte(EcosystemStarknet)
 	copy(byteChainId[len(byteChainId)-len(trimmed):], trimmed)
 	innerChainId, err := newLChainId(byteChainId)
 	if err != nil {
-		return nil, err
+		return StarknetLChainId{}, err
 	}
-	return &StarknetLChainId{
+	return StarknetLChainId{
 		lChainId: *innerChainId,
 	}, nil
 }
 
 // NewStarknetMainnetLChainId returns the Starknet Lombard Chain Id of Starknet mainnet SN_MAIN
 func NewStarknetMainnetLChainId() StarknetLChainId {
-    chId, _ := NewStarknetLChainId("0x534e5f4d41494e")
-    return *chId
+	chId, _ := NewStarknetLChainId("0x534e5f4d41494e")
+	return chId
 }
 
 // NewStarknetSepoliaLChainId returns the Starknet Lombard Chain Id of Starknet sepolia SN_SEPOLIA
 func NewStarknetSepoliaLChainId() StarknetLChainId {
-    chId, _ := NewStarknetLChainId("0x534e5f5345504f4c4941")
-    return *chId
+	chId, _ := NewStarknetLChainId("0x534e5f5345504f4c4941")
+	return chId
 }
 
 // Identifier returns the textual identifier of a Starknet network.
 // They all start with SN
 func (ch StarknetLChainId) Identifier() string {
-    snIndex := bytes.Index(ch.inner[1:], []byte("SN"))
-    if snIndex == -1 {
-        return string(ch.inner[1:])
-    }
-    return string(ch.inner[snIndex+1:])
+	snIndex := bytes.Index(ch.inner[1:], []byte("SN"))
+	if snIndex == -1 {
+		return string(ch.inner[1:])
+	}
+	return string(ch.inner[snIndex+1:])
 }
